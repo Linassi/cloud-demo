cc3520cd4acde11e44e4c958871bbcc9870d51c1
Linassi <1098067622@qq.com>
2022-07-28 22:17:29 +0800

    Project Init.

---
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -0,0 +1,18 @@
+package cn.itcast.order.service;
+
+import cn.itcast.order.mapper.OrderMapper;
+import cn.itcast.order.pojo.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    public Order queryOrderById(Long orderId) {
+        // 1.查询订单
+        return orderMapper.findById(orderId);
+    }
+}
