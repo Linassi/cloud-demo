0d5d17cc99b8557322534a256e65cd19c931ae6a
Linassi <1098067622@qq.com>
2022-07-29 23:11:50 +0800

    利用WebClient进行远程调用

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -2,8 +2,10 @@ package cn.itcast.order.service;
 
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class OrderService {
@@ -11,8 +13,17 @@ public class OrderService {
     @Autowired
     private OrderMapper orderMapper;
 
+    @Autowired
+    private WebClient webClient;
+
     public Order queryOrderById(Long orderId) {
+
         // 1.查询订单
-        return orderMapper.findById(orderId);
+        Order order = orderMapper.findById(orderId);
+        //调用WebClient发送http请求获取user
+        User user = webClient.get().uri("/user/" + order.getUserId())
+            .retrieve().bodyToMono(User.class).block();
+        order.setUser(user);
+        return order;
     }
 }
