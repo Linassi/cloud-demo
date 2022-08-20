0eba8bee87945c262faf9eb503b5c1dcea11c0b7
JJGu <12345678@qq.com>
2022-08-20 11:51:18 +0800

    Feign http客户端

--- order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,11 +1,14 @@
 package cn.itcast.order;
 
+import cn.itcast.order.config.DefaultFeignConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
+@EnableFeignClients(defaultConfiguration = DefaultFeignConfiguration.class)
 public class OrderApplication {
 
     public static void main(String[] args) {
--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,11 +1,11 @@
 package cn.itcast.order.service;
 
+import cn.itcast.order.clients.UserClient;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
@@ -13,12 +13,15 @@ public class OrderService {
     @Autowired
     private OrderMapper orderMapper;
 
+    @Autowired
+    private UserClient userClient;
+
 /*    @Autowired
     private WebClient.Builder webClientBuilder;*/
     /*@Autowired
     private WebClient webClient;*/
 
-    @Autowired
+    /*@Autowired
     private RestTemplate restTemplate;
 
     public Order queryOrderById(Long orderId) {
@@ -27,12 +30,24 @@ public class OrderService {
         Order order = orderMapper.findById(orderId);
 
         //调用WebClient发送http请求获取user
-        /*User user = webClient.get().uri("http://userservice/user/" + order.getUserId())
-            .retrieve().bodyToMono(User.class).block();*/
+        *//*User user = webClient.get().uri("http://userservice/user/" + order.getUserId())
+            .retrieve().bodyToMono(User.class).block();*//*
         User user = restTemplate.getForObject("http://userservice/user/" + order.getUserId(),User.class);
 
         order.setUser(user);
         return order;
 
+    }*/
+    public Order queryOrderById(Long orderId) {
+
+        // 1.查询订单
+        Order order = orderMapper.findById(orderId);
+        //调用Feign发送http请求获取User
+        User user = userClient.findById(order.getUserId());
+        order.setUser(user);
+        return order;
+
     }
+
+
 }
